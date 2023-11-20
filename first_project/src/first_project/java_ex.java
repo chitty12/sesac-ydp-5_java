b75c91475ec7cade56c9628a900aed2a395fc4ae
chitty12 <cty12angel@naver.com>
2023-11-20 17:48:31 +0900

    study: scanner 실습

---
+++ first_project/src/first_project/java_ex.java
@@ -0,0 +1,23 @@
+package first_project;
+
+import java.util.Scanner;
+
+
+public class java_ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이름을 입력하세요");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.println("나이를 입력하세요");
+		int age = sc.nextInt();
+		
+
+
+
+		System.out.println("안녕하세요!" + name + "님(" + age + "세)");
+		sc.close();
+	}
+
+}
