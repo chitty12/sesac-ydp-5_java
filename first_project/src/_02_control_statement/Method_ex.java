efc81a879bef90e29d55e18dbbbd8d3b5fda31a0
chitty12 <cty12angel@naver.com>
2023-11-22 15:29:57 +0900

    study: 메소드, 메소드 오버로딩

---
+++ first_project/src/_02_control_statement/Method_ex.java
@@ -0,0 +1,61 @@
+package _02_control_statement;
+
+import java.util.Scanner;
+
+public class Method_ex {
+
+	public static void main(String[] args) {
+		Method_ex ol = new MethodOverloading();
+		
+		System.out.println("숫자 두개를 입력하세요.");
+		
+		Scanner sc = new Scanner(System.in); 
+
+		int number1 = sc.nextInt();
+		int number2 = sc.nextInt();
+		
+		System.out.println("덧셈결과:" + add(number1, number2));
+		System.out.println("뺄셈결과:" + minus(number1, number2));
+		System.out.println("나눗셈결과:" + dvd(number1, number2));
+		System.out.println("곱셈결과:" + mult(number1, number2));
+		
+		System.out.println("반지름이 5인 원의 넓이:" + ol.result(5));
+		System.out.println("가로4, 세로7 직사각형 넓이:" + ol.result(4, 7));
+		System.out.println("밑변6, 높이 3 삼각형의 넓이:" + ol.result(6, 3, 2));
+		
+
+		
+		
+		sc.close();
+	}
+	
+	public static double add(int a, int b) {
+		return  + a+b;
+
+	}
+	
+	public static double minus(int a, int b) {
+		return  a-b;
+	}
+	
+	public static double dvd(int a, int b) {
+		System.out.println(a/b);
+		return  a/b;
+	}
+	
+	public static  double mult(int a, int b) {
+		return  a*b;
+	}
+
+	public double result(double a) {
+		return Math.PI*(a*a);
+	}
+	
+	public double result(double a, double b) {
+		return a*b;
+	}
+	
+	public double result (double a, double b, double c) {
+		return a*b/c;
+	}
+}
