2483b987751fcc2b0d58550929939b7f311cf4de
chitty12 <cty12angel@naver.com>
2023-11-23 23:19:27 +0900

    exception 실습

--- first_project/src/_04_exception/Exception_Ex.java
+++ first_project/src/_04_exception/Exception_Ex.java
@@ -1,5 +1,7 @@
 package _04_exception;
 
+//import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exception_Ex {
@@ -16,19 +18,38 @@ public class Exception_Ex {
 		}
 		
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("배열크기를 입력해주세요");
-		int i = sc.nextInt();
-		int[] arr1 = new int[i];			
-//		try {
-//		}
-//		
-//		catch(InputMismatchException) {}
-//		NegativeArraySizeException
-		
-		
-		
+		try {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("배열크기를 입력해주세요");
+			int i = sc.nextInt();
+			int[] arr1 = new int[i];			
+			
+			int sum = 0;
+			for (int n: arr1) {
+				sum += n;				
+			}
+			
+			System.out.println("배열의 평균값 :" + (double) sum/i);
+			
+		}
+		 catch(ArithmeticException e){
+			System.out.println("0으로 정수를 나눌수 없어요");
+		}
+		  catch(InputMismatchException e) {
+			System.out.println("배열 크기가 정수값이 아닙니다.");
+			
+		} catch(NegativeArraySizeException e) {
+			System.out.println("배열 크기값은 양수값입니다."); 
+			
+		} catch(NullPointerException e) {
+			System.out.println("null값 입력 오류");
+		} catch(ClassCastException e) {
+			System.out.println("형변환이 불가함.");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열 인덱스의 범위를 벗어남.");
+		}
 		
 		}
 }
